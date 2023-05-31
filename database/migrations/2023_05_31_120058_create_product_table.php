3368483b44463ecdb8eee220dd9a0ec1ffcf303d
Maiziko <maizikog@gmail.com>
2023-05-31 19:19:29 +0700

    add migration

---
+++ database/migrations/2023_05_31_120058_create_product_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 45);
+            $table->integer('price');
+            $table->integer('stok');
+            $table->text('description');
+            $table->unsignedBigInteger('categories_id');
+            $table->foreign('categories_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+};
